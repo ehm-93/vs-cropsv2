aef510e1ea3088829bca92dfabc1d646456e40cb
Emmett Miller <ehm-93@users.noreply.github.com>
2025-05-06 17:37:09 -0700

    feat: add updateMoistureLevel wrapper patches

--- CropsV2/BlockEntity/BlockEntityFarmlandV2.cs
+++ CropsV2/BlockEntity/BlockEntityFarmlandV2.cs
@@ -1,3 +1,5 @@
+using System;
+using HarmonyLib;
 using Vintagestory.API.Common;
 using Vintagestory.GameContent;
 
@@ -5,5 +7,32 @@ namespace Ehm93.VintageStory.CropsV2;
 
 class BlockEntityFarmlandV2 : BlockEntityFarmland
 {
-    
+    private int mulchLevel = 0;
+
+    public int MulchLevel {
+        get => mulchLevel;
+        set => mulchLevel = Math.Clamp(value, 0, 3);
+    }
+
+    [HarmonyPatchCategory("cropsv2")]
+    [HarmonyPatch(typeof(BlockEntityFarmland), "updateMoistureLevel", new Type[] {
+        typeof(double), typeof(float), typeof(bool), typeof(ClimateCondition)
+    })]
+    internal static class UpdateMoistureLevelPatch
+    {
+        [HarmonyPrefix]
+        public static void Before(BlockEntityFarmland __instance, ref float __state)
+        {
+            if (__instance is not BlockEntityFarmlandV2 self) return;
+            __state = self.moistureLevel;
+        }
+
+        [HarmonyPostfix]
+        public static void After(BlockEntityFarmland __instance, ref float __state)
+        {
+            if (__instance is not BlockEntityFarmlandV2 self) return;
+            float diff = __state - self.moistureLevel;
+            self.moistureLevel += 0.25f * self.MulchLevel * diff;
+        }
+    }
 }
--- CropsV2/CropsV2ModSystem.cs
+++ CropsV2/CropsV2ModSystem.cs
@@ -1,9 +1,12 @@
-﻿using Vintagestory.API.Common;
+﻿using HarmonyLib;
+using Vintagestory.API.Common;
 
 namespace Ehm93.VintageStory.CropsV2;
 
 public class CropsV2ModSystem : ModSystem
 {
+  private Harmony patcher;
+  
   public override void Start(ICoreAPI api)
   {
     base.Start(api);
@@ -14,5 +17,27 @@ public class CropsV2ModSystem : ModSystem
     api.RegisterBlockEntityClass("BEFarmlandV2", typeof(BlockEntityFarmlandV2));
 
     api.RegisterItemClass("ItemPlantableSeedV2", typeof(ItemPlantableSeedV2));
+    
+    HarmonyPatch();
+  }
+  
+  public override void Dispose()
+  {
+    HarmonyUnpatch();
+  }
+
+  private void HarmonyPatch()
+  { 
+    // may duplicate if client and server share an instance
+    if (!Harmony.HasAnyPatches(Mod.Info.ModID))
+    {
+      patcher = new Harmony(Mod.Info.ModID);
+      patcher.PatchCategory(Mod.Info.ModID);
+    }
+  }
+
+  private void HarmonyUnpatch()
+  {
+    patcher?.UnpatchAll(Mod.Info.ModID);
   }
 }
